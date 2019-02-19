67c2e29ffc5587e5a9f8d6003a5f91d076b816dc
xavcz <xavier.cazalot@gmail.com>
2019-02-19 10:52:55 +0100

    🎨 Emphasis accepts an accent color

--- posts/001/Emphasis.js
+++ posts/001/Emphasis.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+// prettier-ignore
 const Emphasis = styled.a`
   position: relative;
 
@@ -14,7 +15,10 @@ const Emphasis = styled.a`
     background: #282725;
     opacity: 0.1;
     transform-origin: 50% 50%;
-    transition: transform 0.3s ease-out, opacity 0.3s ease-out, background 0.3s ease-out;;
+    transition:
+      transform 0.3s ease-out,
+      opacity 0.3s ease-out,
+      background 0.3s ease-out;
   }
 
   &:focus,
@@ -22,14 +26,17 @@ const Emphasis = styled.a`
     &:after {
       transform: scaleY(1.5);
       opacity: 0.5;
-      transition: transform 0.3s ease-in, opacity 0.3s ease-in, background 0.3s ease-in;;
+      transition:
+        transform 0.3s ease-in,
+        opacity 0.3s ease-in,
+        background 0.3s ease-in;
     }
   }
 
   &:active {
     &:after {
       transform: scale(0.8, 1.5);
-      background: crimson;
+      ${props => props.accent && `background: ${props.accent}`};
     }
   }
 `;
